57a3868125eaea82a5b90b93e48ac4a042bdb497
eshellster <eshellster@gmail.com>
2024-06-23 20:31:20 +0900

    feat: login

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import User from "../entities/user";
+import jwt from "jsonwebtoken";
 
 const registerUser = async (req: Request, res: Response) => {
   const { name, email, password } = req.body;
@@ -19,8 +20,31 @@ const registerUser = async (req: Request, res: Response) => {
   }
 };
 
-const authenticateUser = (req: Request, res: Response) => {};
+const loginUser = async (req: Request, res: Response) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOneBy({ email });
+    if (!user) {
+      return res.status(400).json({ message: "Invalid email or password" });
+    }
+
+    const isMatch = await user.comparePassword(password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid email or password" });
+    }
+
+    const token = jwt.sign({ id: user.id }, "your_jwt_secret", {
+      expiresIn: "1h",
+    });
+
+    res.json({ token });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error logging in", error });
+  }
+};
 
 const logoutUser = (req: Request, res: Response) => {};
 
-export { registerUser, authenticateUser, logoutUser };
+export { registerUser, loginUser, logoutUser };
